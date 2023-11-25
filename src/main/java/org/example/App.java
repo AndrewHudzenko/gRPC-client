040b537bd877a70afc06866f336fb1885e7335ed
Andrew Hudzenko PC <s1mpleworkpost@gmail.com>
2023-11-25 22:35:52 +0200

    add basic functionality

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
+import com.andersen.grpc.GreetingServiceGrpc;
+import com.andersen.grpc.GreetingServiceOuterClass;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.util.Iterator;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+                .usePlaintext()
+                .build();
+
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub =
+                GreetingServiceGrpc.newBlockingStub(channel);
+
+        GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass.HelloRequest
+                .newBuilder().setFirstName("Andrew")
+                .build();
+
+        GreetingServiceOuterClass.HelloResponse response = stub.greeting(request);
+
+        System.out.println(response);
+
+        channel.shutdownNow();
     }
 }
--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,2 +1,4 @@
-package org.example;public class Client {
+package org.example;
+
+public class Client {
 }
